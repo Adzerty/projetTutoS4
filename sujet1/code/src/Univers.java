81e45817c560e6928038225aaa19176cd2988522
Adzerty <adzerty76@outlook.fr>
2021-03-22 15:43:02 +0100

    Feat : deplacement du vaisseau

---
+++ sujet1/code/src/Univers.java
@@ -0,0 +1,21 @@
+public class Univers
+{
+    private int nbPlanete = 0;
+    public Univers()
+    {
+        FrameNombrePlanetes choixNombre = new FrameNombrePlanetes(this);
+    }
+
+    public void openFrameUniv()
+    {
+        new FrameUnivers(nbPlanete);
+    }
+    public void setNbPlanete(int nbPlanete) {
+        this.nbPlanete = nbPlanete;
+    }
+
+
+    public static void main(String[] args) {
+        new Univers();
+    }
+}
