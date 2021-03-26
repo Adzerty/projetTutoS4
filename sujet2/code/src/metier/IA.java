cb40565cbb6c71006c1c7ab8e5d3f2b6d3e284c6
Adzerty <adzerty76@outlook.fr>
2021-03-26 11:20:34 +0100

    Create IA.java

---
+++ sujet2/code/src/metier/IA.java
@@ -0,0 +1,15 @@
+package metier;
+
+import java.util.ArrayList;
+
+public class IA
+{
+    public static int choixAction( double xVaisseau, double yVaisseau, double accelerationVaisseau, double vitesseActuelle, double angleActuelle,
+                            double xPandora, double yPandora,   double taillePandora,
+                            ArrayList<Planete> alMeteorites)
+    {
+        
+
+        return 0;
+    }
+}
